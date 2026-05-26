b85c64315d36a9dccb7dec68c96734b5b640fc4c
Ashborn <isaigalvan12309@gmail.com>
2026-05-26 15:32:38 -0600

    cambios

--- app/src/main/java/com/example/mibusdemo/principal.kt
+++ app/src/main/java/com/example/mibusdemo/principal.kt
@@ -2,9 +2,11 @@ package com.example.mibusdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 // Implementar OnMapReadyCallback
 class principal : AppCompatActivity(), OnMapReadyCallback {
@@ -23,8 +25,17 @@ class principal : AppCompatActivity(), OnMapReadyCallback {
 
     // 4. Este metodo es llamado cuando el mapa esta listo para ser utilizado
     override fun onMapReady(googleMap: GoogleMap) {
-        // Mover la camara (mapa) a un punto en especifico
-        // Mover a Xalapa ------- PROBAR Y VALIDARRR ----------
-        // googleMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(19.54, -96.91)))
+        // Mover la vita del mapa a Xalapa ------- PROBAR Y VALIDARRR ----------
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(19.54, -96.91)))
+        // Aumentar el zoom del mapa
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+
+        // Mostrar icono de parada en todas las paradas
+
+    }
+
+    private fun mostrarParadasEnMapa(nMapa: GoogleMap) {
+
+
     }
 }
